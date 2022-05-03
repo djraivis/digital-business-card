86635c05134abd87f78395ad17055d3803a9f45b
Raivis <highdoven@gmail.com>
2022-05-03 18:30:00 +0100

    update all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// React
+import React from 'react'
+// Components
+import Card from './components/Card'
+import Whoami from './components/Whoami'
+import Buttons from './components/Buttons'
+import Content from './components/Content'
+import Skills from './components/Skills'
+// Styles
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="digital-business-card">
+      <Card />
+      <Whoami />
+      <Buttons />
+      <Content />
+      <Skills />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
